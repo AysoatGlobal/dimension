642d962a018fad52c84fc8cd7f07abf1f227ec1a
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2022-07-22 15:22:55 +0530

    Notifications & chats added

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `c4bd27eb97885a84e063c9bd35986cd9`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
